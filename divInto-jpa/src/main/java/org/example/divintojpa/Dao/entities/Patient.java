bde38402338b374d5ca024f86e8ed8575f6c5bb4
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-02-28 15:26:24 +0000

    implement les methodes de PatientRepository

--- divInto-jpa/src/main/java/org/example/divintojpa/Dao/entities/Patient.java
+++ divInto-jpa/src/main/java/org/example/divintojpa/Dao/entities/Patient.java
@@ -1,9 +1,6 @@
 package org.example.divintojpa.Dao.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Date;
@@ -14,6 +11,7 @@ public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
+    @Temporal(TemporalType.DATE)
     private Date dateNaissance;
     private boolean malade;
     private int score;
--- divInto-jpa/src/main/java/org/example/divintojpa/DivIntoJpaApplication.java
+++ divInto-jpa/src/main/java/org/example/divintojpa/DivIntoJpaApplication.java
@@ -1,15 +1,50 @@
 package org.example.divintojpa;
 
+import org.example.divintojpa.Dao.entities.Patient;
+import org.example.divintojpa.Repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.List;
 import java.util.logging.ConsoleHandler;
 
 @SpringBootApplication
-public class DivIntoJpaApplication{
+public class DivIntoJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private PatientRepository patientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DivIntoJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		patientRepository.save(new Patient(null, "Hassan", new Date(), false, 15));
+		patientRepository.save(new Patient(null, "Yassine", new Date(), false, 20));
+		patientRepository.save(new Patient(null, "Omar", new Date(), true, 30));
+
+		List<Patient> patients =patientRepository.findAll();
+
+		patients.forEach(p->{
+			System.out.println("-------------------------");
+			System.out.println(p.getNom());
+			System.out.println(p.getDateNaissance());
+			System.out.println(p.getScore());
+			System.out.println(p.isMalade());
+			System.out.println("-------------------------");
+		});
+		System.out.println("//////////////////////////////");
+		patientRepository.findById(1L).ifPresent(p->{
+			System.out.println(p.getNom());
+			System.out.println(p.getDateNaissance());
+			System.out.println(p.getScore());
+			System.out.println(p.isMalade());
+		});
+
 	}
 
 }
