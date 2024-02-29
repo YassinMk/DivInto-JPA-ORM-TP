ae81a42f9d1a0524fe4b0a499df7c85dda5e1f46
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-02-29 23:59:50 +0000

    add file Jpa-Orm-relational to test realtion between entities

---
+++ divInto-Jpa-ORM-RelationalDB/src/main/java/org/example/divintojpaormrelationaldb/DivIntoJpaOrmRelationalDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.divintojpaormrelationaldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivIntoJpaOrmRelationalDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivIntoJpaOrmRelationalDbApplication.class, args);
+    }
+
+}
---
+++ divInto-Jpa-ORM-RelationalDB/src/test/java/org/example/divintojpaormrelationaldb/DivIntoJpaOrmRelationalDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.divintojpaormrelationaldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivIntoJpaOrmRelationalDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
