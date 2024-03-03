ed56c644276ec8a4074e13d442618aff3b69330f
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-03 23:41:26 +0000

    prepare envirement

---
+++ divInto-JPA-ORM-hospitalApp/src/main/java/org/example/divintojpaormhospitalapp/DivIntoJpaOrmHospitalAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.divintojpaormhospitalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivIntoJpaOrmHospitalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivIntoJpaOrmHospitalAppApplication.class, args);
+    }
+
+}
---
+++ divInto-JPA-ORM-hospitalApp/src/test/java/org/example/divintojpaormhospitalapp/DivIntoJpaOrmHospitalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.divintojpaormhospitalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivIntoJpaOrmHospitalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
